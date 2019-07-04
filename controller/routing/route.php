1ddd942dba405d5e5739835ed5a010b6ef734064
Johnny Pacheco <johnnye96pv@gmail.com>
2019-07-03 20:54:04 -0500

    Inicio de optimizacion del routing

--- controller/routing/route.php
+++ controller/routing/route.php
@@ -2,47 +2,50 @@
 class Route {
 	protected $uri;
 	protected $closure;
-	// protected $uriPattern;
+	protected $parameters;
+	public $isStaticFile;
+
 	const PARAMETER_PATTERN = '/:([^\/]+)/';
 	const PARAMETER_REPLACEMENT = '(?<\1>[^/]+)';
-	protected $parameters;
-	public function __construct($uri, $closure)
-	{
+	
+
+	public function __construct($uri, $closure, $isStaticFile){
 		$this->uri = $uri;
 		$this->closure = $closure;
+		$this->isStatic = $isStatic;
 	}
-	public function getUriPattern()
-	{
+
+	public function getUriPattern(){
 		$uriPattern = preg_replace(self::PARAMETER_PATTERN, self::PARAMETER_REPLACEMENT, $this->uri);
 		$uriPattern = str_replace('/', '\/', $uriPattern);
 		$uriPattern = '/^' . $uriPattern . '\/*$/s';
 		return $uriPattern;
 	}
-	public function getParameterNames()
-	{
+
+	public function getParameterNames(){
 		preg_match_all(self::PARAMETER_PATTERN, $this->uri, $parameterNames);
 		return array_flip($parameterNames[1]);
 	}
-	public function resolveParameters($matches)
-	{
+
+	public function resolveParameters($matches){
 		$this->parameters = array_intersect_key($matches, $this->getParameterNames());
 	}
-	public function getParameters()
-	{
+
+	public function getParameters(){
 		return $this->parameters;
 	}
-	public function checkIfMatch($requestUri)
-	{
+
+	public function checkIfMatch($requestUri){
 		$uriPattern = $this->getUriPattern();
-		if (preg_match($uriPattern, $requestUri, $matches))
-		{
+
+		if (preg_match($uriPattern, $requestUri, $matches)){
 			$this->resolveParameters($matches);
 			return true;
 		}
 		return false;
 	}
-	public function execute()
-	{
+
+	public function execute(){
 		$closure = $this->closure;
 		$parameters = $this->getParameters();
 		return call_user_func_array($closure, $parameters);
